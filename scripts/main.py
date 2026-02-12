563970c494b5d8baae843bc3c722ec73e8d9eb4c
sportism <sportismtv@gmail.com>
2026-02-12 18:37:06 +0100

    update script to not fetch unsupported channels

--- scripts/main.py
+++ scripts/main.py
@@ -56,71 +56,81 @@ if is_json(response.text):
     result = data["result"][bein_sport_format_date]
     today_matches = []
     bein_sport_media_url = "https://prod-media.beinsports.com/image/{}.png"
-    repo_url = "https://gitlab.com/sportism/sportism-backend"
-    servers_base_url = "{}/-/raw/main/channels-streaming-urls/{}.json?ref_type=heads"
+    # repo_url = "https://gitlab.com/sportism/sportism-backend"
+    # servers_base_url = "{}/-/raw/main/channels-streaming-urls/{}.json?ref_type=heads"
+
+    repo_url = "https://raw.githubusercontent.com/sportism/sportism-hidden-api"
+    servers_base_url = "{}/refs/heads/main/channels-streaming-urls/{}.json"
 
     def channel_name_convertor(channe_name: str) -> str:
         return "-".join(channe_name.lower().split())
 
+    unsupported_channels = ["xtra", "max", "en", "fr"]
+
+    # print(json.dumps(result))
+
     for i, obj in enumerate(result):
-        for key1, value1 in obj.items():
-            if key1 in "matchTime":
-                match = {
-                    # "eventExternalId": "2486593803668",
-                    # "eventTitle": "Al-Wakrah vs Al Ahli",
-                    # "eventDateTime": "2026-02-06T14:35:00.000Z",
-                    # "eventEndDateTime": "2026-02-06T16:45:00.000Z",
-                    # "sport": "soccer_data",
-                    #
-                    # "channelRedirectUrl": "https://beinconnect.app/beINSPORTS3",
-                    # "description": "Al Wakrah vs Al Ahli",
-                    # "homeTeamName": "Wakrah",
-                    # "awayTeamName": "Ahli",
-                    # "homeTeamCode": "WAK",
-                    # "awayTeamCode": "AHL",
-                    #
-                    # "motosportsData": "",
-                    # "localDateTime": "2026-02-06T15:35:00.000Z"
-                    #
-                    "category": obj["category"],
-                    #
-                    "channelName": obj["channelName"],
-                    "channelExternalId": "94130ea17023c4837f0dcdda95034b65",
-                    "channelLogo": bein_sport_media_url.format(
-                        obj["channelExternalId"]
-                    ),
-                    "channelServers": servers_base_url.format(
-                        repo_url, channel_name_convertor(obj["channelName"])
-                    ),
-                    #
-                    "competitionName": obj["competitionName"],
-                    "competitionOptaId": obj["competitionOptaId"],
-                    "competitionLogo": bein_sport_media_url.format(
-                        obj["competitionOptaId"]
-                    ),
-                    "matchOptaId": obj["matchOptaId"],
-                    #
-                    "homeTeamOptaId": obj["homeTeamOptaId"],
-                    "homeTeamColor": obj["homeTeamColor"],
-                    "homeTeamfullName": obj["homeTeamfullName"],
-                    "homeTeamLogo": bein_sport_media_url.format(obj["homeTeamOptaId"]),
-                    #
-                    "awayTeamOptaId": obj["awayTeamOptaId"],
-                    "awayTeamColor": obj["awayTeamColor"],
-                    "awayTeamfullName": obj["awayTeamfullName"],
-                    "awayTeamLogo": bein_sport_media_url.format(obj["awayTeamOptaId"]),
-                    #
-                    "round": "Regular Season",
-                    "matchDate": obj["matchDate"],
-                    "matchTime": obj["matchTime"],
-                }
-                if "bein sports" in match["channelName"].lower():
-                    today_matches.append(match)
+        match = {
+            # "eventExternalId": "2486593803668",
+            # "eventTitle": "Al-Wakrah vs Al Ahli",
+            # "eventDateTime": "2026-02-06T14:35:00.000Z",
+            # "eventEndDateTime": "2026-02-06T16:45:00.000Z",
+            # "sport": "soccer_data",
+            #
+            # "channelRedirectUrl": "https://beinconnect.app/beINSPORTS3",
+            # "description": "Al Wakrah vs Al Ahli",
+            # "homeTeamName": "Wakrah",
+            # "awayTeamName": "Ahli",
+            # "homeTeamCode": "WAK",
+            # "awayTeamCode": "AHL",
+            #
+            # "motosportsData": "",
+            # "localDateTime": "2026-02-06T15:35:00.000Z"
+            #
+            "category": obj["category"],
+            #
+            "channelName": obj["channelName"],
+            "channelExternalId": "94130ea17023c4837f0dcdda95034b65",
+            "channelLogo": bein_sport_media_url.format(obj["channelExternalId"]),
+            "channelServers": servers_base_url.format(
+                repo_url, channel_name_convertor(obj["channelName"])
+            ),
+            #
+            "competitionName": obj["competitionName"],
+            "competitionOptaId": obj["competitionOptaId"],
+            "competitionLogo": bein_sport_media_url.format(obj["competitionOptaId"]),
+            "matchOptaId": obj["matchOptaId"],
+            #
+            "homeTeamOptaId": obj["homeTeamOptaId"],
+            "homeTeamColor": obj["homeTeamColor"],
+            "homeTeamfullName": obj["homeTeamfullName"],
+            "homeTeamLogo": bein_sport_media_url.format(obj["homeTeamOptaId"]),
+            #
+            "awayTeamOptaId": obj["awayTeamOptaId"],
+            "awayTeamColor": obj["awayTeamColor"],
+            "awayTeamfullName": obj["awayTeamfullName"],
+            "awayTeamLogo": bein_sport_media_url.format(obj["awayTeamOptaId"]),
+            #
+            "round": "Regular Season",
+            "matchDate": obj["matchDate"],
+            "matchTime": obj["matchTime"],
+        }
+
+        channel_name = match["channelName"].lower()
+        is_match_broadcating_on_supporting_channel = (
+            not any(
+                channel_suffix in channel_name
+                for channel_suffix in unsupported_channels
+            )
+            and "bein sports" in channel_name
+        )
+
+        if is_match_broadcating_on_supporting_channel:
+            today_matches.append(match)
 
     with open("../data/live-events.json", "w") as file:
         json.dump(today_matches, file)
 
+    print(json.dumps(today_matches))
 else:
     print("response is not valid (is not json)")
-
-# print(json.dumps(today_matches))
